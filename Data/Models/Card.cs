076b526b40cef6c5b5ec23f63cf38d9dda32dfc3
Jeff Nicholas <jeffery.m.nicholas@gmail.com>
2021-08-24 18:57:51 -0500

    Initial Commit

---
+++ Data/Models/Card.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyTCGCollection.Data.Models
+{
+    [Table("Cards")]
+    public class Card
+    {
+        [Key]
+        [Required]
+
+        public int Id { get; set; }
+        public string CardNumber { get; set; }
+        public string CardName { get; set; }
+        public string CardRarity { get; set; }
+        public int Quantity { get; set; }
+        public string CardValue { get; set; }
+        [ForeignKey(nameof(Game))]
+        public int GameId { get; set; }
+        public string Expansion { get; set; }
+
+
+    }
+}
---
+++ Data/Models/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyTCGCollection.Data.Models
+{
+    [Table("Games")]
+    public class Game
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        public string GameName { get; set; }
+        public virtual List<Card> Cards { get; set; }
+
+    }
+}
